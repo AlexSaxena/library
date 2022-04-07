723775a7f85d2c284780d54942b18c422af83c93
AlexSaxena <saxenaalex@gmail.com>
2022-04-07 17:30:56 +0200

    Adds Psudo & Plans for working project

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Greeting, General Kenobi!");
+
+/* 
+Psudo
+    A Simple library Application (Book Saving)
+
+    Goal :  In this App you should be able to view and fill in books that you have
+            either read or not yet read.
+            To do this there should be a *Form to fill in about Book details
+            A button to display this form
+            A "Card" div representing every added book
+            This Card - Should show all data about book
+                        Should be able to delete book
+                        Should be able to change Read Status
+    
+    Code : Using HTML & CSS
+           Structure the page in minimum 2 parts
+           Header Section & Content / Card Section
+
+           JS - Using Objects and prototypes 
+           Book Object and Constructor for creating a Book
+           All books will be stored in an Array
+           Will need a Function to add book to array
+
+           Will need a function that loops through Array and displays books in cards
+           Will need Button(OnClick) to show Add book Form
+           Will need Button on Card that changes Read Status
+           Will need Button on Card to remove said Book/Card (Need to associate with correct card <-> Book)
+                      
+*/
