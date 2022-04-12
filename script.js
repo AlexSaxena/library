7801834faf9928368255828b92f1b8e5ee684008
AlexSaxena <saxenaalex@gmail.com>
2022-04-12 16:37:56 +0200

    Adds Function for Creating new BookCard

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ function Book(Author, Title, Pages, isRead) {
 // Function for adding Book to Library Array
 function addBook(Author, Title, Pages, isRead = false) {
   const newBook = new Book(Author, Title, Pages, isRead);
+  createCard(newBook.Author, newBook.Title, newBook.Pages, newBook.isRead); // Wrong place
   myLibrary.push(newBook);
 }
 
@@ -66,10 +67,86 @@ function showBooks() {
 
 // Function for generating a book Card
 // Adds book object as param. book.author etc
-function createCard() {
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("card-book");
-  newDiv.innerText = "test";
+function createCard(Author, Title, Pages, isRead) {
+  let cardDiv = document.createElement("div");
+  let cardDivUpper = document.createElement("div");
+  let cardDivLower = document.createElement("div");
+  //let span = document.createElement("span");
 
-  cardContainer.appendChild(newDiv);
+  cardDiv.classList.add("card-book");
+  cardDivUpper.classList.add("card-inner-top");
+  cardDivLower.classList.add("card-inner-bottom");
+
+  let authorSpan = document.createElement("span");
+  authorSpan.textContent = `Author: ${Author}`;
+
+  let titleSpan = document.createElement("span");
+  titleSpan.textContent = `Title: ${Title}`;
+
+  let pagesSpan = document.createElement("span");
+  pagesSpan.textContent = `Pages: ${Pages}`;
+
+  let isReadSpan = document.createElement("span");
+  isReadSpan.textContent = `Status: ${isRead}`;
+
+  let toggleSpan = document.createElement("span");
+  toggleSpan.textContent = "Toggle Soon";
+
+  cardDivUpper.append(authorSpan, titleSpan, pagesSpan, isReadSpan);
+  cardDivLower.append(toggleSpan);
+
+  // let newTextNode = document.createTextNode();
+
+  // let spanAuthor = (span.innerHTML = `Author: ${Author}`);
+  // let spanTitle = (span.innerHTML = `Title: ${Title}`);
+  // let spanPages = (span.innerHTML = `Pages: ${Pages}`);
+  // let spanIsRead = (span.innerHTML = `Read: ${isRead}`);
+
+  // let nodeAuthor = document.createTextNode(`Author: ${Author}`);
+  // let spanAuthor = span;
+  // spanAuthor.appendChild(nodeAuthor);
+
+  // let nodeTitle = document.createTextNode(`Title: ${Title}`);
+  // let spanTitle = span;
+  // spanTitle.appendChild(nodeTitle);
+
+  // let nodePages = document.createTextNode(`Pages: ${Pages}`);
+  // let spanPages = span;
+  // spanPages.appendChild(nodePages);
+
+  // let nodeIsRead = document.createTextNode(`Read: ${isRead}`);
+  // let spanIsRead = span;
+  // spanIsRead.appendChild(nodeIsRead);
+
+  // let spanToggle = (span.textContent = "Toggle Soon");
+
+  // let nodeToggle = document.createTextNode(`Toggle Soon`);
+  // let spanToggle = span;
+  // spanToggle.appendChild(nodeToggle);
+  // Toggle button / or similar
+
+  // cardDivUpper.appendChild(spanAuthor);
+  // cardDivUpper.appendChild(spanTitle);
+  // cardDivUpper.appendChild(spanPages);
+  // cardDivUpper.appendChild(spanIsRead);
+  // cardDiv.appendChild(cardDivUpper);
+
+  // cardDivLower.appendChild(spanToggle);
+  // cardDiv.appendChild(cardDivLower);
+  cardDiv.append(cardDivUpper, cardDivLower);
+  cardContainer.appendChild(cardDiv);
 }
+
+/*
+  <div class="card-book">
+    <div class="card-inner-top">
+      <span>Title</span>
+      <span>Author</span>
+      <span>Pages</span>
+      <span>Status</span>
+    </div>
+    <div class="card-inner-bottom">
+      <span>Toggle-bar</span>
+    </div>
+  </div>
+*/
